1d73b9a5a081bb55287ad9c8c43a1dbe074bc843
harshasilva <nhks2004@gmail.com>
2025-06-12 00:26:31 +0530

    last

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ function hideshow()
     if (display == 1) {
         div.style.display = "block";
         display = 0;
-        
+          document.getElementById("More").innerHTML="Less";
+    
         
     } else {
         div.style.display = "none";
         display = 1;
-        
+        document.getElementById("More").innerHTML="More";
     }
 
 }
